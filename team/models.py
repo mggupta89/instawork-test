9d6e90315bbed0ac4652ffb229d06e5dcf9ad314
mayankgupta <mggupta.89@gmail.com>
2018-12-01 23:27:36 +0530

    Adding team models

--- team/models.py
+++ team/models.py
@@ -2,5 +2,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+#from exceptions.exceptions import InstaworkException
+
+
+class Team(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.EmailField(unique=True)
+    phone = models.IntegerField()
+    role = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if self.role not in settings.INSTA_ROLES:
+            #raise InstaworkException('Invalid role')
+            pass
+        super(Team, self).save(*args, **kwargs)
+
+    def to_json(self):
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'role': self.role
+        }
+
+
